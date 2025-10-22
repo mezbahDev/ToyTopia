7d41f510c8474e435c92aa92569756a96bd44b44
mezbahDev <mezbahuddin9029@gmail.com>
2025-10-23 04:32:19 +0600

    file updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
 import { createRoot } from "react-dom/client";
 import AuthProvider from "./provider/AuthProvider";
-import { CartProvider } from "./context/CartContext"; // ✅ Import CartProvider
-
+import { CartProvider } from "./context/CartContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
